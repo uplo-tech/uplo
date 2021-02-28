07d13c8ad256af2143c926c2dfa6a43c3de8269e
DavidMGilbert <>
2021-02-28 15:13:02 +1100

    Signed-off-by: DavidMGilbert <>

--- types/constants.go
+++ types/constants.go
@@ -370,8 +370,13 @@ func init() {
 		// per year by 12.
 		FoundationSubsidyFrequency = BlocksPerYear / 12
 
-		InitialFoundationUnlockHash = MustParseAddress("053b2def3cbdd078c19d62ce2b4f0b1a3c5e0ffbeeff01280efb1f8969b2f5bb4fdc680f0807")
-		InitialFoundationFailsafeUnlockHash = MustParseAddress("27c22a6c6e6645802a3b8fa0e5374657438ef12716d2205d3e866272de1b644dbabd53d6d560")
+		//InitialFoundationUnlockHash = MustParseAddress("053b2def3cbdd078c19d62ce2b4f0b1a3c5e0ffbeeff01280efb1f8969b2f5bb4fdc680f0807")
+		//InitialFoundationFailsafeUnlockHash = MustParseAddress("27c22a6c6e6645802a3b8fa0e5374657438ef12716d2205d3e866272de1b644dbabd53d6d560")
+		initialFoundationUnlockConditions, _ := GenerateDeterministicMultisig(2, 3, InitialFoundationTestingSalt)
+		initialFoundationFailsafeUnlockConditions, _ := GenerateDeterministicMultisig(3, 5, InitialFoundationFailsafeTestingSalt)
+		InitialFoundationUnlockHash = initialFoundationUnlockConditions.UnlockHash()
+		InitialFoundationFailsafeUnlockHash = initialFoundationFailsafeUnlockConditions.UnlockHash()
+
 
 		// A block time of 1 block per 10 minutes is chosen to follow Bitcoin's
 		// example. The security lost by lowering the block time is not
@@ -469,12 +474,17 @@ func init() {
 		OakMaxRise = big.NewRat(1004, 1e3)
 		OakMaxDrop = big.NewRat(1e3, 1004)
 
-		GenesisUplocoinAllocation = []UplocoinOutput{}
+		GenesisUplocoinAllocation = []UplocoinOutput{
+			{
+				Value:      NewCurrency64(10000000),
+				UnlockHash: UnlockHash{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			},
+		}
 
 		GenesisUplofundAllocation = []UplofundOutput{
 			{
 				Value:      NewCurrency64(100000),
-				UnlockHash: UnlockHash{4, 57, 229, 188, 127, 20, 204, 245, 211, 167, 232, 130, 208, 64, 146, 62, 69, 98, 81, 102, 221, 7, 123, 100, 70, 107, 199, 113, 121, 26, 198, 252},
+				UnlockHash: UnlockHash{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 			},
 		}
 	}
